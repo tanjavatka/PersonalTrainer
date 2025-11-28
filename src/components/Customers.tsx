d1c3a9a1fcc43352312b646efea067b7439952a9
tanjavatka <tanja.vatka@hotmail.com>
2025-11-28 17:12:18 +0200

    OSA 1: haettu kaikki customers ja trainings tiedot

--- src/components/Customers.tsx
+++ src/components/Customers.tsx
@@ -1,23 +1,34 @@
 import { DataGrid, type GridColDef } from "@mui/x-data-grid";
+import { useEffect, useState } from "react";
 
 function Customers() {
 
+    const [customers, setCustomers] = useState([]);
+
+    useEffect(() => {
+        fetch("https://customer-rest-service-frontend-personaltrainer.2.rahtiapp.fi/api/customers")
+            .then(response => response.json())
+            .then(data => setCustomers(data._embedded.customers))
+    }, []);
+
+    console.log(customers);
+
     const columns: GridColDef[] = [
-        { field: "firstname", width: 150, headerName: "First name" },
-        { field: "lastname", width: 150, headerName: "Last name" },
-        { field: "email", width: 150, headerName: "Email" },
-        { field: "phone", width: 150, headerName: "Phone" },
-        { field: "address", width: 150, headerName: "Address" },
-        { field: "postcode", width: 150, headerName: "Postcode" },
-        { field: "city", width: 150, headerName: "City" },
+        { field: "firstname", width: 130, headerName: "First name" },
+        { field: "lastname", width: 130, headerName: "Last name" },
+        { field: "email", width: 200, headerName: "Email" },
+        { field: "phone", width: 120, headerName: "Phone" },
+        { field: "address", width: 200, headerName: "Address" },
+        { field: "postcode", width: 100, headerName: "Postcode" },
+        { field: "city", width: 120, headerName: "City" },
     ]
 
     return (
         <div>
             <DataGrid
-                //rows={}
+                rows={customers}
                 columns={columns}
-            //getRowId={(row) => row.id}
+                getRowId={(row) => row._links.self.href}
             />
         </div>
     )
--- src/components/Trainings.tsx
+++ src/components/Trainings.tsx
@@ -1,6 +1,16 @@
 import { DataGrid, type GridColDef } from "@mui/x-data-grid";
+import { useEffect, useState } from "react";
 
 function Trainings() {
+    const [trainings, setTrainings] = useState([]);
+
+    useEffect(() => {
+        fetch("https://customer-rest-service-frontend-personaltrainer.2.rahtiapp.fi/api/trainings")
+            .then(response => response.json())
+            .then(data => setTrainings(data._embedded.trainings))
+    }, []);
+
+    console.log(trainings);
 
     const columns: GridColDef[] = [
         { field: "activity", width: 200, headerName: "Activity" },
@@ -13,9 +23,9 @@ function Trainings() {
     return (
         <div>
             <DataGrid
-                //rows={}
+                rows={trainings}
                 columns={columns}
-            //getRowId={(row) => row.id}
+                getRowId={(row) => row._links.self.href}
             />
         </div>
     )
